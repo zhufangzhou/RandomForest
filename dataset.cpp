017dde5d9ebfc0f643a050494066182e28d6a5b1
zhufangzhou <zhu.ark@gmail.com>
2014-09-15 13:20:27 +0800

    compile success

--- dataset.cpp
+++ dataset.cpp
@@ -1,38 +1,52 @@
 #include "dataset.h"
 
 Dataset::Dataset() {
+	// initialize X and y, otherwise will error when call realloc
 	X = NULL;
 	y = NULL;
 }
 
 Dataset::~Dataset() {
-	if (X != NULL)
-		delete[] X;
-	if (y != NULL)
-		delete[] y;
+	delete[] X;
+	delete[] y;
 }
 
 
 void Dataset::readBinary(std::string filename, int feature_size) {
 	FILE* fp = fopen(filename.c_str(), "rb");
 	int i = 0;
+	double *X_buf, y_buf;
+	X_buf = new double[feature_size];
+
 	timer.tic();
 	std::cout << "Start reading dataset from " << filename << std::endl;
 
-	while (fread(y + i, sizeof(double), 1, fp)) {
-		if (fread(X + i*feature_size, sizeof(double), feature_size, fp) != feature_size) {
+	// first read a sample into buffer
+	while (fread(&y_buf, sizeof(double), 1, fp)) {	
+		if (fread(X_buf, sizeof(double), feature_size, fp) != feature_size) {
 			std::cout << "dataset size is wrong, missing several values" << std::endl;
 			exit(EXIT_FAILURE);
 		}
+		
+		// sample size add one
 		i++;
+		// expand space to store current sample
+		X = (double*) realloc(X, sizeof(double)*i*feature_size);
+		y = (double*) realloc(y, sizeof(double)*i);
+		// copy buffer to dataset
+		memcpy(X + (i-1)*feature_size, X_buf, sizeof(double)*feature_size);
+		memcpy(y + (i-1), &y_buf, sizeof(double));
 	}
 	
 	std::cout << "Finish reading dataset. Sample size: " << i << ", Feature size: " << feature_size << "." << std::endl;
 	timer.toc();
 
+	// set sample size and feature size
 	this->sample_size = i;
 	this->feature_size = feature_size;
 	
+	// delete buffer
+	delete[] X_buf;
 	fclose(fp);
 }
 
@@ -40,18 +54,25 @@ void Dataset::readBinary(std::string feature_filename, std::string label_filenam
 	FILE *fp_feature = fopen(feature_filename.c_str(), "rb");
 	FILE *fp_label = fopen(label_filename.c_str(), "rb");
 	int i = 0, j = 0;
+	double *X_buf;
 	size_t read_count;
 	
-	timer.tic();
-	std::cout << "Start reading dataset from " << filename << std::endl;
+	X_buf = (double*) malloc(sizeof(double)*feature_size);
 
-	while (read_count = fread(X + i*feature_size, sizeof(double), feature_size, fp_feature)) {
+	timer.tic();
+	std::cout << "Start reading dataset from " << feature_filename << " and " << label_filename << std::endl;
+
+	while (read_count = fread(X_buf, sizeof(double), feature_size, fp_feature)) {
 		if(read_count != feature_size) {
 			std::cout << "dataset size is wrong, missing several values" << std::endl;
 			exit(EXIT_FAILURE);
 		}
 		i++;
+		X = (double*) realloc(X, sizeof(double)*i*feature_size);
+		memcpy(X + (i-1)*feature_size, X_buf, sizeof(double)*feature_size);
 	}
+
+	y = (double*) malloc(sizeof(double)*i);
 	if (fread(y, sizeof(double), i, fp_label) != i) {
 		std::cout << "label file sample size dismatch with feature file." << std::endl;
 		exit(EXIT_FAILURE);
@@ -63,14 +84,15 @@ void Dataset::readBinary(std::string feature_filename, std::string label_filenam
 	this->sample_size = i;
 	this->feature_size = feature_size;
 
+	delete[] X_buf;
 	fclose(fp_feature);
 	fclose(fp_label);
 }
 
 void Dataset::readText(std::string filename, int feature_size) {
-	FILE *fp = fopen(filename.c_str(), "r");
+	/*FILE *fp = fopen(filename.c_str(), "r");
 	int i;
-
+	const 
 	timer.tic();
 	std::cout << "Start reading dataset from " << filename << std::endl;
 	
@@ -87,6 +109,6 @@ void Dataset::readText(std::string filename, int feature_size) {
 	this->sample_size = i;
 	this->feature_size = feature_size;
 	
-	fclose(fp);
+	fclose(fp);*/
 }
 
