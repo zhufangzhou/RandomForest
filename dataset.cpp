8ae2c39a1c4e38d3fa58d072273af0f342821917
zhufangzhou <zhu.ark@gmail.com>
2014-11-19 09:49:45 +0800

    rewrite dataset

--- dataset.cpp
+++ dataset.cpp
@@ -1,276 +1,72 @@
 #include "dataset.h"
-#include "utils.h"
 
-Dataset::Dataset() {
-	// initialize X and y, otherwise will error when call realloc
-	X = NULL;
-	y = NULL;
-	discrete_idx = NULL;
-	discrete_size = 0;
-	discrete_mask = NULL;	
+example_t::example_t() {
+	nnz = 0;
+	y = -1;
+	fea_id = new int[1];
+	fea_value = new feature_t[1];
 }
 
-Dataset::~Dataset() {
-	delete[] X;
-	delete[] y;
-	delete[] discrete_mask;
+example_t::~example_t() {
+	delete[] fea_id;
+	felete[] fea_value;
 }
 
-void Dataset::reset() {
-	delete[] X;
-	delete[] y;
-	sample_size = 0;
-	feature_size = 0;
+void example_t::push_back(int id, feature_t value) {
+	fea_id = (int*)realloc(fea_id, nnz+1);
+	fea_value = (feature_t*)realloc(fea_value, nnz+1);
+
+	fea_id[nnz] = id;
+	fea_value[nnz] = value;
+
+	nnz++;
 }
 
-void Dataset::set_dataset(Dataset* ds, bool is_copy) {
-	set_dataset(ds->X, ds->y, ds->sample_size, ds->feature_size, is_copy, ds->discrete_idx, ds->discrete_size);	
-}
-
-void Dataset::set_dataset(double *X, double *y, int sample_size, int feature_size, bool is_copy, int *discrete_idx, int discrete_size) {
-	set_dataset(X, sample_size, feature_size, is_copy, discrete_idx, discrete_size);
-	if (y == NULL)
-		throw "dataset.cpp::set_dataset-->\n\tVector `y` must not be NULL";
-	// do we copy the y into the dataset or just point to it
-	if (!is_copy) {
-		this->y = y;
-	} else {
-		this->y = new double[sample_size];
-		memcpy(this->y, y, sizeof(double)*sample_size);
-	}
-}
-
-void Dataset::set_dataset(double *X, int sample_size, int feature_size, bool is_copy, int *discrete_idx, int discrete_size) {
-	// first reset the dataset
-	reset();
-	// initialize dataset
-	this->sample_size = sample_size;
-	this->feature_size = feature_size;
-	
-	if (X == NULL)
-		throw "dataset.cpp::set_dataset-->\n\tVector `X` must not be NULL";
-	// do we copy the X into the dataset or just point to it
-	if (!is_copy) {
-		this->X = X;
-	} else {
-		this->X = new double[sample_size*feature_size];
-		memcpy(this->X, X, sizeof(double)*sample_size*feature_size);
-	}
-
-	// deal with discrete feature
-	if (discrete_idx != 0 && discrete_idx != NULL)
-		handle_discrete_feature(discrete_idx, discrete_size);
-}
-
-void Dataset::handle_discrete_feature(int *discrete_idx, int discrete_size) {
-	// use set to obatain unique discrete values
-	std::unordered_set<discrete_t> ss;
-	int counts = 0;
-
-	
-	
-	if (!(discrete_idx == 0 || discrete_idx == NULL)) {
-		// determine which features is discrete
-		discrete_mask = new int[feature_size];
-		memset(discrete_mask, 0xff, sizeof(int)*feature_size);
-		
-		this->discrete_idx = discrete_idx;
-		this->discrete_size = discrete_size;
-		
-		this->discrete_value = new discrete_t*[discrete_size];
-		for (int j = 0; j < discrete_size; j++) {
-			// update the discrete mask, if -1 is continuous then the index of discrete_value
-			discrete_mask[discrete_idx[j]] = j;
-			// clear the unordered_set for a new discrete feature
-			ss.clear();
-			// insert all the feature value of this discrete feature into unordered_set and get distinct value
-			for (int i = 0; i < sample_size; i++) {
-				ss.insert((discrete_t) X[i*feature_size + discrete_idx[j]]);
-			}
-			discrete_value[j] = new discrete_t[ss.size() + 1];
-			// the first element for each discrete feature is its distinct value count --> discrete_value[j][0]
-			discrete_value[j][0] = 0;
-			// iterate the unordered_set to get all the distinct value for this discrete feature
-			for (auto it = ss.begin(); it != ss.end(); it++) {
-				discrete_value[j][(int)++discrete_value[j][0]] = *it;
-			}
-		}
-	}
-	delete[] discrete_mask;
-}
-
-void Dataset::readBinary(std::string filename, int feature_size, bool is_train, int *discrete_idx, int discrete_size) {
-	FILE* fp = fopen(filename.c_str(), "rb");
-	int i = 0;
-	double *X_buf, y_buf;
-	size_t read_count;
-	
-	// first reset the dataset
-	reset();
-	
-	X_buf = new double[feature_size];
-
-	timer.tic();
-	std::cout << "Start reading dataset from " << filename << std::endl;
-
-	if(is_train) {
-		// first read a sample into buffer
-		while (fread(&y_buf, sizeof(double), 1, fp)) {	
-			if (fread(X_buf, sizeof(double), feature_size, fp) != feature_size) {
-				std::cerr << "dataset size is wrong, missing several values" << std::endl;
-				exit(EXIT_FAILURE);
-			}
-			
-			// sample size add one
-			i++;
-			// expand space to store current sample
-			X = (double*) realloc(X, sizeof(double)*i*feature_size);
-			y = (double*) realloc(y, sizeof(double)*i);
-			// copy buffer to dataset
-			memcpy(X + (i-1)*feature_size, X_buf, sizeof(double)*feature_size);
-			memcpy(y + (i-1), &y_buf, sizeof(double));
-		}
-	} else {
-		while (!(read_count = fread(X_buf, sizeof(double), feature_size, fp))) {
-			if(read_count != feature_size) {
-				std::cout << "dataset size is wrong, missing several values" << std::endl;
-				exit(EXIT_FAILURE);
-			}
-			i++;
-			X = (double*) realloc(X, sizeof(double)*i*feature_size);
-			memcpy(X + (i-1)*feature_size, X_buf, sizeof(double)*feature_size);
-		}
-	}
-	
-	std::cout << "Finish reading dataset. Sample size: " << i << ", Feature size: " << feature_size << "." << std::endl;
-	timer.toc();
-
-	// set sample size and feature size
-	this->sample_size = i;
-	this->feature_size = feature_size;
-	
-	// deal with discrete feature
-	handle_discrete_feature(discrete_idx, discrete_size);
-
-	// delete buffer
-	delete[] X_buf;
-	fclose(fp);
-}
-
-void Dataset::readBinary(std::string feature_filename, std::string label_filename, int feature_size,
-						 int *discrete_idx, int discreate_size) {
-	FILE *fp_feature = fopen(feature_filename.c_str(), "rb");
-	FILE *fp_label = fopen(label_filename.c_str(), "rb");
-	int i = 0, j = 0;
-	double *X_buf;
-	size_t read_count;
-	
-	// first reset the dataset
-	reset();
-
-	X_buf = (double*) malloc(sizeof(double)*feature_size);
-
-	timer.tic();
-	std::cout << "Start reading dataset from " << feature_filename << " and " << label_filename << std::endl;
-
-	while (!(read_count = fread(X_buf, sizeof(double), feature_size, fp_feature))) {
-		if(read_count != feature_size) {
-			std::cout << "dataset size is wrong, missing several values" << std::endl;
-			exit(EXIT_FAILURE);
-		}
-		i++;
-		X = (double*) realloc(X, sizeof(double)*i*feature_size);
-		memcpy(X + (i-1)*feature_size, X_buf, sizeof(double)*feature_size);
-	}
-
-	y = (double*) malloc(sizeof(double)*i);
-	if (fread(y, sizeof(double), i, fp_label) != i) {
-		std::cout << "label file sample size dismatch with feature file." << std::endl;
+DataReader::DataReader(std::string filename, int n_features) {
+	ifs.open(filename.c_str(), ios::binary);
+	if (ifs == NULL) {
+		std::cerr << "Can not open file " << filename << " ." << std::endl;
 		exit(EXIT_FAILURE);
 	}
-
-	std::cout << "Finish reading dataset. Sample size: " << i << ", Feature size: " << feature_size << "." << std::endl;
-	timer.toc();
-
-	this->sample_size = i;
-	this->feature_size = feature_size;
-
-	// deal with discrete feature
-	handle_discrete_feature(discrete_idx, discrete_size);
-
-	delete[] X_buf;
-	fclose(fp_feature);
-	fclose(fp_label);
+	this->n_features = n_features;
 }
 
-void Dataset::readText(std::string filename, int feature_size, bool is_train, int *discrete_idx, int discreate_size) {
-	FILE *fp = fopen(filename.c_str(), "r");
-	const int MAX_LINE = feature_size * 20;
-	const char* DELIMITER = " ";
-	double *X_buf = new double[feature_size], y_buf;
-	char *line = new char[MAX_LINE], *pch;
-	int i = 0, start;
+example_t* DataReader::read_an_example() {
+	example_t* ret;
+	std::string line, t_str;
+	int p_pos, c_pos, feature_id;
+	feature_t feature_value;
 
-	// first reset the dataset
-	reset();
-
-	std::cout << "Start reading dataset from " << filename << std::endl;
-	timer.tic();
-	
-	// read a line and split to get label and each feature value	
-	while (fgets(line, MAX_LINE, fp)) {
-		pch = strtok(line, DELIMITER);			// read label
-		if (pch == NULL || !strcmp(pch,"")) {
-			std::cerr << "Line " << i+1 << " miss label." << std::endl;
-			exit(EXIT_FAILURE);
-		}
-		if (is_train) {
-			y_buf = atof(pch);
-			start = 0;
-		} else {								// predict do not need to read label
-			X_buf[0] = atof(pch);
-			start = 1;
-		}
-
-		// read features
-		for (int j = start; j < feature_size - 1; j++) {
-			pch = strtok(NULL, DELIMITER);
-			if (pch == NULL || !strcmp(pch, "")) {
-				std::cerr << "Line " << i+1 << " miss feature" << "." << std::endl;
-				exit(EXIT_FAILURE);
-			}
-			X_buf[j] = atof(pch);
-		}
-		// read last feature
-		pch = strtok(NULL, "\n");
-		if (pch == NULL || !strcmp(pch, "")) {
-			std::cerr << "Line " << i+1 << " miss feature" << "." << std::endl;
-			exit(EXIT_FAILURE);
-		}
-		X_buf[feature_size-1] = atof(pch);
-
-		// sample size add one
-		i++;
-		// expand space to store current sample
-		X = (double*) realloc(X, sizeof(double)*i*feature_size);
-		y = (double*) realloc(y, sizeof(double)*i);
-		// copy buffer to dataset
-		memcpy(X + (i-1)*feature_size, X_buf, sizeof(double)*feature_size);
-		memcpy(y + (i-1), &y_buf, sizeof(double));
+	if (ifs.eof()) {
+		return NULL;
 	}
 
-	std::cout << "Finish reading dataset. Sample size: " << i << ", Feature size: " << feature_size << "." << std::endl;
+	ret = new example_t();
 	
-	timer.toc();
+	ifs >> ret->y;
+	getline(ifs, line);
+	p_pos = 0;
+	c_pos = line.find(' ', 0);
 	
-	this->sample_size = i;
-	this->feature_size = feature_size;
+	while (p_pos <= c_pos) {
+		p_pos = c_pos + 1;
+		c_pos = line.find(':', p_pos);
+		t_str = line.substr(p_pos, c_pos - p_pos);
+		feature_id = atoi(t_str.c_str()) - startIndex;
 
-	// deal with discrete feature
-	handle_discrete_feature(discrete_idx, discrete_size);
-	
-	delete[] line;
-	delete[] X_buf;
-	fclose(fp);
+		p_pos = c_pos + 1;
+		c_pos = line.find(' ', p_pos);
+		feature_value = atof(line.substr(p_pos, c_pos - p_pos));
+
+		ret->push_back(feature_id, feature_value);
+	}
+	return ret;	
 }
 
+ev_pair_t* DataReader::read_examples() {
+	example_t* single = new example_t();
+	ev_pair_t
+	while( (single=read_an_example()) != NULL ) {
+
+	}
+}
--- dataset.h
+++ dataset.h
@@ -1,43 +1,83 @@
+/**
+ * @file dataset.h
+ * @brief 
+ * @author Zhu Fangzhou, zhu.ark@gmail.com
+ * @version 1.0
+ * @date 2014-11-18
+ */
 #ifndef __DATASET_HEADER
 #define __DATASET_HEADER
 
-#include <cstdlib>
-#include <unordered_set>
+#include <string>
 
-#define discrete_t double
-#define TRAIN true
-#define PREDICT false
+typedef short target_t;
+typedef float feature_t;
+enum learn_mode {TRAIN, PREDICT};
 
-class Dataset {	
-private:
-	void handle_discrete_feature(int *discrete_idx, int discrete_size);
-	void reset();
-public:
-	int sample_size;						// sample size
-	int feature_size;						// feature size
-	double *X;								// sample
-	double *y;								// label
+typedef struct {
+	int ex_id;  /** example id */
+	feature_t fea_value; /** feature value */
+}ev_pair_t;
 
-	// discrete feature
-	int *discrete_mask;						// not -1 --> discrete feature, -1 --> continuous feature
-	discrete_t **discrete_value;			// discrete feature value list
-	int *discrete_idx;						// discrete feature idx array
-	int discrete_size;						// discreate feature size
-
-	// constructor
-	Dataset();
-	~Dataset();
-	
-	void set_dataset(double *X, double *y, int sample_size, int feature_size, bool is_copy, int *discrete_idx = NULL, int discrete_size = 0);
-	void set_dataset(double *X, int sample_size, int feature_size, bool is_copy, int *discrete_idx = NULL, int discrete_size = 0);
-	void set_dataset(Dataset* ds, bool is_copy);
-
-	void readText(std::string filename, int feature_size, bool is_train, int *discrete_idx = NULL, int discrete_size = 0);
-	
-	void readBinary(std::string filename, int feature_size, bool is_train, int *discrete_idx = NULL, int discrete_size = 0);
-	void readBinary(std::string feature_filename, std::string label_filename, int feature_size, 
-					int *discrete_idx = NULL, int discrete_size = 0);
+class example_t {
+	public:
+		target_t y;
+		int nnz;
+		int* fea_id;
+		feature_t* fea_value;
+		example_t();
+		~example_t();
+		void push_back(int id, feature_t value);
 };
 
+class DataReader {
+	private:
+		int n_features;	
+		std::ifstream ifs;
+	public:
+		/**
+		 * @brief DataReader constructor
+		 *
+		 * @param n_features number of features
+		 */
+		DataReader(std::string filename, int n_features);
+		/**
+		 * @brief read_an_example read an example
+		 *
+		 * @param ifs input file stream to read example
+		 *
+		 * @return a single example's features
+		 */
+		example_t* read_an_example();		
+		/**
+		 * @brief read_examples read all the example
+		 *
+		 * @param filename 
+		 *
+		 * @return a sparse matrix contains all examples' features
+		 */
+		ev_pair_t** read_examples();
+};
+
+class Dataset {
+	private:
+		ev_pair_t** x; 		/** each row is an attribute */	
+		int* size; 			/** number of examples with non-zero feature value for each attribute */
+		target_t* y; 			/** label for each example */
+		
+		int n_classes; 		/** number of classes */
+		int n_examples;		/** number of examples */
+		int n_features; 	/** number of attributes */
+
+		bool* is_cate; 		/** is the ith attribute categorical */
+	public:
+		/**
+		 * @brief load_data 
+		 *
+		 * @param filename
+		 * @param mode
+		 */
+		void load_data(std::string& filename, learn_mode mode);
+};
 
 #endif
