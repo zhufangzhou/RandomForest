f7a0918909a4937f337631064b9cff6010399e5d
zhufangzhou <zhu.ark@gmail.com>
2014-11-19 13:54:11 +0800

    update dataset

--- dataset.cpp
+++ dataset.cpp
@@ -1,3 +1,10 @@
+/**
+ * @file dataset.cpp
+ * @brief 
+ * @author Zhu Fangzhou, zhu.ark@gmail.com
+ * @version 1.0
+ * @date 2014-11-19
+ */
 #include "dataset.h"
 
 example_t::example_t() {
@@ -9,7 +16,7 @@ example_t::example_t() {
 
 example_t::~example_t() {
 	delete[] fea_id;
-	felete[] fea_value;
+	delete[] fea_value;
 }
 
 void example_t::push_back(int id, feature_t value) {
@@ -22,13 +29,31 @@ void example_t::push_back(int id, feature_t value) {
 	nnz++;
 }
 
-DataReader::DataReader(std::string filename, int n_features) {
-	ifs.open(filename.c_str(), ios::binary);
-	if (ifs == NULL) {
+void example_t::debug() {
+	if (y != -1) {
+		std::cout << "Example Label: " << y << std::endl;
+	}
+	std::cout << "Features: " << std::endl;
+	for (int i = 0; i < nnz; i++) {
+		std::cout << fea_id[i] << ":" << fea_value[i] << " ";
+	}
+	std::cout << std::endl << std::endl;
+}
+
+DataReader::DataReader(const std::string& filename, int n_features, const learn_mode mode) {
+	ifs.open(filename.c_str(), std::ios::binary);
+	if (!ifs.is_open()) {
 		std::cerr << "Can not open file " << filename << " ." << std::endl;
 		exit(EXIT_FAILURE);
 	}
 	this->n_features = n_features;
+	this->mode = mode;
+}
+
+DataReader::~DataReader() {
+	if (ifs.is_open()) {
+		ifs.close();
+	}
 }
 
 example_t* DataReader::read_an_example() {
@@ -43,30 +68,96 @@ example_t* DataReader::read_an_example() {
 
 	ret = new example_t();
 	
-	ifs >> ret->y;
-	getline(ifs, line);
-	p_pos = 0;
-	c_pos = line.find(' ', 0);
+	if (mode == TRAIN) {
+		ifs >> ret->y;
+		p_pos = 0;
+		getline(ifs, line);
+		c_pos = line.find(' ', 0);
+	} else {
+		p_pos = 0;
+		c_pos = 0;
+		getline(ifs, line);
+	}
 	
 	while (p_pos <= c_pos) {
 		p_pos = c_pos + 1;
 		c_pos = line.find(':', p_pos);
 		t_str = line.substr(p_pos, c_pos - p_pos);
-		feature_id = atoi(t_str.c_str()) - startIndex;
+		feature_id = atoi(t_str.c_str());
 
 		p_pos = c_pos + 1;
 		c_pos = line.find(' ', p_pos);
-		feature_value = atof(line.substr(p_pos, c_pos - p_pos));
+		feature_value = atof(line.substr(p_pos, c_pos - p_pos).c_str());
+
+		if (feature_id >= n_features) {
+			std::cerr << "input file feature id " << feature_id << " exceed `n_features` " << n_features << std::endl;
+			exit(EXIT_FAILURE);
+		}
 
 		ret->push_back(feature_id, feature_value);
+	}
+
+	/* if read a blank line, just skip it */
+	if (ret->nnz == 0) {
+		return NULL;
 	}
 	return ret;	
 }
 
-ev_pair_t* DataReader::read_examples() {
-	example_t* single = new example_t();
-	ev_pair_t
-	while( (single=read_an_example()) != NULL ) {
+std::vector<example_t*> DataReader::read_examples() {
+	example_t* single;
+	std::vector<example_t*> ret;
 
+	while( (single=read_an_example()) != NULL ) {
+		ret.push_back(single);
 	}
+
+	return ret;
 }
+
+Dataset::Dataset() {
+	is_init = false;
+}
+
+Dataset::Dataset(int n_classes, int n_features) {
+	init(n_classes, n_features);
+}
+
+Dataset::~Dataset() {
+	delete[] x;
+	delete[] x[0];
+	delete[] y;
+	delete[] is_cate;	
+}
+
+void Dataset::init(int n_classes, int n_features) {
+	this->n_classes = n_classes;
+	this->n_features = n_features;
+	x = new ev_pair_t*[this->n_features];
+	x[0] = new ev_pair_t[1];
+
+	is_cate = new bool[this->n_features];
+	is_init = true;
+}
+
+void Dataset::load_data(const std::string& filename, const learn_mode mode) {
+	DataReader* dr = new DataReader(filename, n_features, mode);
+	std::vector<example_t*> ex_vec;
+	m_timer* t = new m_timer();
+
+	if (!is_init) {
+		std::cerr << "Please init the Dataset first" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	t->tic("Loading data from file "+filename+" ...");
+	ex_vec = dr->read_examples();
+	t->toc("Done.");
+
+	t->tic("Generating dataset ...");
+	for (auto it = ex_vec.begin(); it != ex_vec.end(); it++) {
+			
+	}
+	t->toc("Done.");
+}
+
--- dataset.h
+++ dataset.h
@@ -5,14 +5,20 @@
  * @version 1.0
  * @date 2014-11-18
  */
-#ifndef __DATASET_HEADER
-#define __DATASET_HEADER
+#pragma once
 
+#include <iostream>
 #include <string>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
 
-typedef short target_t;
-typedef float feature_t;
-enum learn_mode {TRAIN, PREDICT};
+#include "utils.h"
+
+typedef short target_t; 	/** label data type */
+typedef float feature_t; 	/** feature data type */
+enum learn_mode {TRAIN, PREDICT}; 	/** `train` mode or `predict` mode */
 
 typedef struct {
 	int ex_id;  /** example id */
@@ -21,26 +27,49 @@ typedef struct {
 
 class example_t {
 	public:
-		target_t y;
-		int nnz;
-		int* fea_id;
-		feature_t* fea_value;
+		target_t y; /** example label*/
+		int nnz; 	/** number of non-zero attribute in this example */
+		int* fea_id; 	/** array of non-zero feature id */
+		feature_t* fea_value; /** array of non-zero feature value */
+
+		/**
+		 * @brief example_t constructor
+		 */
 		example_t();
+		/**
+		 * @brief ~example_t deconstructor
+		 */
 		~example_t();
+		/**
+		 * @brief push_back push an entry to this example
+		 *
+		 * @param id feature id
+		 * @param value feature value
+		 */
 		void push_back(int id, feature_t value);
+		/**
+		 * @brief debug print information
+		 */
+		void debug();
 };
 
 class DataReader {
 	private:
-		int n_features;	
-		std::ifstream ifs;
+		int n_features;		/** number of features in the input file */
+		std::ifstream ifs; 		/** input file stream related to the input file */
+		learn_mode mode; 	/** learn mode */
 	public:
 		/**
 		 * @brief DataReader constructor
 		 *
 		 * @param n_features number of features
+		 * @param mode train or predict
 		 */
-		DataReader(std::string filename, int n_features);
+		DataReader(const std::string& filename, int n_features, const learn_mode mode);
+		/**
+		 * @brief ~DataReader deconstructor
+		 */
+		~DataReader();
 		/**
 		 * @brief read_an_example read an example
 		 *
@@ -54,30 +83,47 @@ class DataReader {
 		 *
 		 * @param filename 
 		 *
-		 * @return a sparse matrix contains all examples' features
+		 * @return a vector contains all examples' features
 		 */
-		ev_pair_t** read_examples();
+		std::vector<example_t*> read_examples();
 };
 
 class Dataset {
 	private:
 		ev_pair_t** x; 		/** each row is an attribute */	
 		int* size; 			/** number of examples with non-zero feature value for each attribute */
-		target_t* y; 			/** label for each example */
+		target_t* y; 		/** label for each example */
 		
 		int n_classes; 		/** number of classes */
 		int n_examples;		/** number of examples */
 		int n_features; 	/** number of attributes */
 
 		bool* is_cate; 		/** is the ith attribute categorical */
+
+		bool is_init;
 	public:
 		/**
-		 * @brief load_data 
-		 *
-		 * @param filename
-		 * @param mode
+		 * @brief Dataset 
 		 */
-		void load_data(std::string& filename, learn_mode mode);
+		Dataset();
+		/**
+		 * @brief init 
+		 *
+		 * @param n_classes
+		 * @param n_features
+		 */
+		void init(int n_classes, int n_features);
+		/**
+		 * @brief ~Dataset deconstructor
+		 */
+		~Dataset();
+		/**
+		 * @brief load_data generate the dataset from input file
+		 *
+		 * @param filename input file name
+		 * @param mode `TRAIN` or `PREDICT`
+		 */
+		void load_data(const std::string& filename, const learn_mode mode);
+		void load_data_meta(const std::string& filename);
 };
 
-#endif
